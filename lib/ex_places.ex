83409480bd9a2fe3127efee6bb0c17c6fafc6728
Sina Karimi <creamycode@gmail.com>
2016-10-19 14:59:38 +1100

    ExPlaces base module

---
+++ lib/ex_places.ex
@@ -0,0 +1,36 @@
+defmodule ExPlaces do
+  @moduledoc """
+  An Elixir API client for the Google Maps Places API
+
+  ## Usage
+
+  ```
+  def deps
+    [{:ex_places, "~> 0.1"}]
+  ```
+
+  Add the `:ex_places` application as your list of applications in `mix.exs`:
+
+  ```elixir
+  def applications do
+    [applications: [:logger, :ex_places]]
+  end
+  ```
+  """
+
+  use Application
+
+  def start(_,_) do
+    import Supervisor.Spec, warn: false
+
+    children = []
+
+    opts = [strategy: :one_for_one, name: ExPlaces.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  @spec places_autocomplete(String.t) :: {atom, map}
+  def places_autocomplete(input) do
+    Request.places_autocomplete(input)
+  end
+end
