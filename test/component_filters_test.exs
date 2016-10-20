37f56d4b45cda7aed3fe584fc62a8a54eaf6ed1f
Sina Karimi <creamycode@gmail.com>
2016-10-20 15:00:36 +1100

    Component filter module

---
+++ test/component_filters_test.exs
@@ -0,0 +1,11 @@
+defmodule ExPlaces.ComponentFiltersTest do
+  use ExUnit.Case, async: false
+
+  alias ExGeocode.ComponentFilters
+
+  test "serialize component filters" do
+    filters = %ComponentFilters{ route: "Test Street", country: "AU" }
+
+    assert ComponentFilters.serialize(filters) == "country:AU|route:Test Street"
+  end
+end
